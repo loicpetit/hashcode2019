3c4eca71f6b805bd8a74f901b384c593ae0777b4
Etienne Alif <etienne.alif@capgemini.com>
2019-02-28 20:27:53 +0100

    IScorable implemented

--- src/Models/Photo.cs
+++ src/Models/Photo.cs
@@ -1,14 +1,18 @@
+using hashcode2019.src.Services;
 using System;
 using System.Collections.Generic;
 
-namespace hashcode2019 {
+namespace hashcode2019
+{
 
-    public class Photo
+    public class Photo : IScorable
     {
-        public int Id { get;set; }
+        public int Id { get; set; }
         public bool IsHorizontal { get; set; }
         public int NumberOfTags => Tags.Count;
-        public List<string> Tags { get; set; } = new List<string>();
+        public IList<string> Tags { get; set; } = new List<string>();
+
+        public IList<Tuple<IScorable, int>> Scores { get; set; } = new List<Tuple<IScorable, int>>();
 
         public override string ToString()
         {
--- src/Models/Slide.cs
+++ src/Models/Slide.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using hashcode2019.src.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace hashcode2019.src.Models
 {
-    public class Slide
+    public class Slide : IScorable
     {
         public Photo Photo1 { get; }
         public Photo Photo2 { get; }
+
+        public IList<string> Tags { get; set; } = new List<string>();
+        public IList<Tuple<IScorable, int>> Scores { get; set; } = new List<Tuple<IScorable, int>>();
 
         public Slide(Photo p1)
         {
